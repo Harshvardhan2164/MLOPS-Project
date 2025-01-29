cbffba63228cf1201bc1ea80d301826bf178dbc5
Harshvardhan Sharma <harshvardhan22102@iiitnr.edu.in>
2025-01-29 22:59:02 +0530

    Data Validation Added

--- End-to-End ML Project/main.py
+++ End-to-End ML Project/main.py
@@ -1,11 +1,23 @@
 from MLproject import logger
 from MLproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from MLproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
 try:
     logger.info(f">>>>>>> Stage {STAGE_NAME} started <<<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> Stage {STAGE_NAME} completed <<<<<<<<\n\nx=============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(f">>>>>>> Stage {STAGE_NAME} started <<<<<<<")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>> Stage {STAGE_NAME} completed <<<<<<<<\n\nx=============x")
 except Exception as e:
