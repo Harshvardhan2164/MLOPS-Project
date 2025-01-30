b7ec60ce6eea5c75449d6dae8c3d706a5a67e0cb
Harshvardhan Sharma <harshvardhan22102@iiitnr.edu.in>
2025-01-30 16:57:55 +0530

    Data Transformation Added

--- End-to-End ML Project/main.py
+++ End-to-End ML Project/main.py
@@ -1,6 +1,7 @@
 from MLproject import logger
 from MLproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from MLproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from MLproject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -22,4 +23,14 @@ try:
     logger.info(f">>>>>>> Stage {STAGE_NAME} completed <<<<<<<<\n\nx=============x")
 except Exception as e:
     logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Tranformation stage"
+
+try:
+    logger.info(f">>>>>>> Stage {STAGE_NAME} started <<<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> Stage {STAGE_NAME} completed <<<<<<<<\n\nx=============x")
+except Exception as e:
     raise e
---
+++ End-to-End ML Project/src/MLproject/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,38 @@
+import os
+from pathlib import Path
+from MLproject import logger
+from MLproject.components.data_transformation import DataTransformation
+from MLproject.config.configuration import ConfigurationManager
+
+STAGE_NAME = "Data Transformation Stage"
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+    
+    def main(self):
+        try:
+            with open(Path("artifacts/data_validation/status.txt", "r")) as f:
+                status = f.read().split(" ")[-1]
+                
+            if status == True:
+                config = ConfigurationManager()
+                data_transformation_config = config.get_data_transformation_config()
+                data_transformation = DataTransformation(config=data_transformation_config)
+                data_transformation.train_test_splitting()
+            else:
+                raise Exception("Your data schema is not valid")
+            
+        except Exception as e:
+            print(e)
+        
+        
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>>> Stage {STAGE_NAME} started <<<<<<<")
+        obj = DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>> Stage {STAGE_NAME} completed <<<<<<<<\n\nx=============x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
