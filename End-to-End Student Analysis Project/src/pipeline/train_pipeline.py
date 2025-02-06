25f9581168370a4f816070624d208ded8042ccd8
Harshvardhan Sharma <harshvardhan22102@iiitnr.edu.in>
2025-02-06 15:24:58 +0530

    Data Transformation Added

---
+++ End-to-End Student Analysis Project/src/pipeline/train_pipeline.py
@@ -0,0 +1,18 @@
+from src.exception import CustomException
+from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
+
+class Pipeline:
+    def __init__(self):
+        pass
+    
+    def main(self):
+        obj = DataIngestion()
+        train_data, test_data = obj.initiate_data_ingestion()
+        
+        data_transformation = DataTransformation()
+        train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr, test_arr))
